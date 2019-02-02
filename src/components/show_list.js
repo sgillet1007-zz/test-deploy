0c82ca98dcf0773758728bec64fc66589e9a273d
sgillet1007 <samuel.p.gillet@gmail.com>
2019-02-01 21:25:54 -0700

    update show details

--- src/components/show_list.js
+++ src/components/show_list.js
@@ -6,9 +6,10 @@ export default () => {
         <div>
             <DividerHeading headerText={'Upcoming Shows'} />
             <div>
-                <strong>Boulder Film Festival</strong> Boulder, CO 
+                <strong>Saturday, March 2 9:00pm-12:30am</strong>
+                <div>License #1 at the Hotel Boulderado (BIFF)</div>
                 <div>
-                    Saturday March, 2nd @ 9pm
+                    <a href="https://biff1.com/music/" rel="noopener noreferrer" target="_blank">2115 13th Street, Boulder, CO 80302</a>
                 </div>
             </div>
         </div>
