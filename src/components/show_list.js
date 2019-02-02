29ba92d8c1b58567a72626e2221eb517b1fb77a2
sgillet1007 <samuel.p.gillet@gmail.com>
2019-02-01 21:29:11 -0700

    add link

--- src/components/show_list.js
+++ src/components/show_list.js
@@ -7,9 +7,11 @@ export default () => {
             <DividerHeading headerText={'Upcoming Shows'} />
             <div>
                 <strong>Saturday, March 2 9:00pm-12:30am</strong>
-                <div>License #1 at the Hotel Boulderado (BIFF)</div>
                 <div>
-                    <a href="https://biff1.com/music/" rel="noopener noreferrer" target="_blank">2115 13th Street, Boulder, CO 80302</a>
+                    <a href="https://biff1.com/music/" rel="noopener noreferrer" target="_blank">License #1 at the Hotel Boulderado (BIFF)</a>
+                </div>
+                <div>
+                    <a href="https://goo.gl/maps/uAV4ShiL9nF2" rel="noopener noreferrer" target="_blank">2115 13th Street, Boulder, CO 80302</a>
                 </div>
             </div>
         </div>
