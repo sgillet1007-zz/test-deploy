7ef1ca9238b984514a4df6be669631efc37f1fb0
sgillet1007 <samuel.p.gillet@gmail.com>
2019-02-13 19:01:46 -0700

    add yt link

--- src/components/media_page.js
+++ src/components/media_page.js
@@ -3,6 +3,7 @@ import DividerHeading from './divider_heading'
 import Photos from './photos'
 import fb from '../images/social/fb.jpg'
 import ig from '../images/social/ig.jpg'
+import yt from '../images/social/yt.jpg'
 
 const Media = () => (
     <div>
@@ -11,16 +12,23 @@ const Media = () => (
             <Photos />
             <DividerHeading headerText={'Social'} />
             <div>
-                <h3>Like us on Facebook!</h3>
+                <h3>Like</h3>
                 <a href="https://www.facebook.com/theconstanttourists/" target='_blank' rel="noopener noreferrer">
                     <img className="social-link" src={fb} height="70px" alt="facebook" />
                 </a>
             </div>
             <br />
             <div>
-                <h3>Follow us on Instagram!</h3>
+                <h3>Follow</h3>
                 <a href="https://www.instagram.com/theconstanttourists/" target='_blank' rel="noopener noreferrer">
-                    <img className="social-link" src={ig} height="70px" alt="facebook" />
+                    <img className="social-link" src={ig} height="70px" alt="instagram" />
+                </a>
+            </div>
+            <br />
+            <div>
+                <h3>Subscribe</h3>
+                <a href="https://www.youtube.com/channel/UCgfAsHmyzHzDL9d_kSDYEPg" target='_blank' rel="noopener noreferrer">
+                    <img className="social-link" src={yt} height="70px" alt="youtube" />
                 </a>
             </div>
     </div>
