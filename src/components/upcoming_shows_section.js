cd0a38a003775ef8c16b93bd5dc81ad1b10c757a
sgillet1007 <samuel.p.gillet@gmail.com>
2019-07-19 17:53:03 -0600

    update gigs

--- src/components/upcoming_shows_section.js
+++ src/components/upcoming_shows_section.js
@@ -41,23 +41,23 @@ const shows = [
     //     // address: '4115 W 38th Ave, Denver, CO 80212',
     //     // address_url: 'https://goo.gl/maps/112npp9z67pDNpGG7'
     // },
-    {
-        when: 'Wednesday, July 17th 7pm - 8pm',
-        venue: 'Littleton Museum - Summer Concert Series',
-        venue_url: 'https://www.littletongov.org/city-services/city-departments/museum',
-        address: '6028 S Gallup St, Littleton, CO 80120',
-        address_url: 'https://goo.gl/maps/aob3tjAv6mw'
-    },
+    // {
+    //     when: 'Wednesday, July 17th 7pm - 8pm',
+    //     venue: 'Littleton Museum - Summer Concert Series',
+    //     venue_url: 'https://www.littletongov.org/city-services/city-departments/museum',
+    //     address: '6028 S Gallup St, Littleton, CO 80120',
+    //     address_url: 'https://goo.gl/maps/aob3tjAv6mw'
+    // },
     {
         when: 'Sunday, July 21st 6pm - 8pm',
         venue: 'Odell Brewing Company',
-        venue_url: 'https://www.odellbrewing.com/',
-        address: '800 E Lincoln Ave, Fort Collins, CO 80524',
-        address_url: 'https://goo.gl/maps/2i2o3Z6NSiQfDfnR7'
+        // venue_url: 'https://www.odellbrewing.com/',
+        address: 'Private Event',
+        // address_url: 'https://goo.gl/maps/2i2o3Z6NSiQfDfnR7'
     },
     {
         when: 'Sunday, July 28th 7:30pm - 10pm',
-        venue: 'Private Event - Boulder, CO',
+        venue: 'Wedding Reception- Boulder, CO',
     },
     {
         when: 'Saturday, August 24th 12pm - 3pm',
