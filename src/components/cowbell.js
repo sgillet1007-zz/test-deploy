df722326e8d47191055f6dffd99d6989651f5145
Samuel Gillet <samuel.p.gillet@gmail.com>
2020-03-30 21:14:02 -0600

    fix audio track

--- src/components/cowbell.js
+++ src/components/cowbell.js
@@ -1,37 +1,37 @@
 import React, { Component } from 'react';
-import cowbell_image from '../images/cowbell.png'
-import DividerHeading from './divider_heading'
+import cowbell_audio from '../tracks/cowbell.mp3';
+import cowbell_image from '../images/cowbell.png';
+import DividerHeading from './divider_heading';
 
 class Cowbell extends Component {
-    componentDidMount() {
-        document.addEventListener('keydown', this.handleKeyPress);
-      }
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
 
-      componentWillUnmount() {
-        document.removeEventListener('keydown', this.handleKeyPress);
-      }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
 
-      handleKeyPress = e => {
-        if (e.keyCode === 32) {
-          this.playCowbell();
-        }
-      }
-
-      playCowbell = () => {
-        var cb = new Audio('http://dight310.byu.edu/media/audio/FreeLoops.com/2/2/Cowbell%20Hit-8994-Free-Loops.com.mp3')
-        cb.play()
+  handleKeyPress = e => {
+    if (e.keyCode === 32) {
+      this.playCowbell();
     }
+  };
 
-    render = () => (
-        <div>
-            <DividerHeading headerText={'Cowbell'} />
-            <div onClick={this.playCowbell}>
-                <img src={cowbell_image} height={175} alt="cowbell" /> 
-                <p>Click or press spacebar to add more cowbell</p>
-            </div>
-        </div>
-    )
+  playCowbell = () => {
+    var cb = new Audio(cowbell_audio);
+    cb.play();
+  };
+
+  render = () => (
+    <div>
+      <DividerHeading headerText={'Cowbell'} />
+      <div onClick={this.playCowbell}>
+        <img src={cowbell_image} height={175} alt='cowbell' />
+        <p>Click or press spacebar to add more cowbell</p>
+      </div>
+    </div>
+  );
 }
 
 export default Cowbell;
-
