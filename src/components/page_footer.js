d388c2b3fecc6537afddf243293b49896a431af9
Samuel Gillet <samuel.p.gillet@gmail.com>
2020-05-02 10:39:57 -0600

    update footer

--- src/components/page_footer.js
+++ src/components/page_footer.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import email from '../images/icons/email.png'
-import phone from '../images/icons/phone.png'
+import React from "react";
+import email from "../images/icons/email.png";
+import phone from "../images/icons/phone.png";
 
 export default () => (
-    <div id='page-footer'>
-        <a id="tel" href={atob('dGVsOjcyMDcwNzUxMDE=')}>
-            <img src={phone} alt='phone' />
-        </a>
-        <a id="email" href={atob('bWFpbHRvOmJvb2tpbmdAdGhlY29uc3RhbnR0b3VyaXN0cy5jb20=')}>
-            <img src={email} alt='email' />
-        </a>
-        <br />
-        <small>
-            <div>Copyright The Constant Tourists.</div>
-            <div>All rights reserved.</div>
-        </small>
-    </div>
-)
+  <div id="page-footer">
+    <a id="tel" href={atob("dGVsOjcyMDcwNzUxMDE=")}>
+      <img src={phone} alt="phone" />
+    </a>
+    <a
+      id="email"
+      href={atob("bWFpbHRvOmJvb2tpbmdAdGhlY29uc3RhbnR0b3VyaXN0cy5jb20=")}
+    >
+      <img src={email} alt="email" />
+    </a>
+    <br />
+    <small>
+      <div>The Constant Tourists</div>
+      <div>© {new Date().getFullYear()} All Rights Reserved</div>
+    </small>
+  </div>
+);
